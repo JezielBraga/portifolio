67ed531ae50de90a695ab4b78bfdd12ac1276452
Jeziel Braga <jeziel@dominio.com.br>
2022-12-29 12:15:19 -0300

    :loud_sound: Show conference information in browser logs.

--- scripts/contas.js
+++ scripts/contas.js
@@ -14,17 +14,19 @@ function formatToCurrency(value) {
 };
 
 function calcularContribuicao() {
-    let ok = false,
+    let repeat = true,
         cont1,
         cont2,
         cont1e2,
+        ultPerc,
         { divida, renda1, renda2 } = getValues();
 
     function calcularPercentual(renda, perc) {
         return renda * perc / 100;
     };
 
-    for (let p = 0; ok == false; p++) {
+    for (let p = 0; repeat; p++) {
+        ultPerc = p;
         cont1 = calcularPercentual(renda1, p);
         cont2 = calcularPercentual(renda2, p);
         cont1e2 = (cont1 + cont2).toFixed(2);
@@ -32,30 +34,31 @@ function calcularContribuicao() {
         if (cont1e2 > divida) {
             let perc = p;
 
-            for (let p = 1; ok == false; p++) {
+            for (let p = 1; repeat; p++) {
+                ultPerc = perc - p / 100;
                 cont1 = calcularPercentual(renda1, perc - p / 100);
                 cont2 = calcularPercentual(renda2, perc - p / 100);
                 cont1e2 = ((cont1 + cont2).toFixed(2));
 
-                if (cont1e2 <= divida) {
-                    ok = true;
-                    console.log(p);
-                }
+                if (cont1e2 <= divida)
+                    repeat = false;
 
                 //trava de segurança
                 if (p == 100)
-                    ok = true;
+                    repeat = false;
             };
         };
 
         //trava de segurança
         if (p == 100)
-            ok = true;
+            repeat = false;
     };
 
     console.clear();
 
-    console.log(cont1e2);
+    console.log(`A soma das contribuições é R$ ${cont1e2}`);
+
+    console.log(`O percentual aplicado foi de ${ultPerc}%`);
 
     document.querySelector("#cont-rend-1")
         .textContent = formatToCurrency(cont1);
