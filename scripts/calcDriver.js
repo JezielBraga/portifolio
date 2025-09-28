09827dee3f306c72c6bc0158cde924c1c309814e
Jeziel Braga <jeziel.std@gmail.com>
2025-09-28 13:05:55 -0300

    Atualiza identificadores e ajusta funcionalidades gerais

--- scripts/calcDriver.js
+++ scripts/calcDriver.js
@@ -1,48 +1,34 @@
 function toNumber(value) {
-    return parseFloat(value.replace(",", "."));
+    return value == ""
+        ? 0
+        : parseFloat(value.replace(",", "."));
 };
 
 function getValues() {
     return [
-        toNumber(document.querySelector("#fatur").value),
+        document.querySelector("#ganhos").value,
         toNumber(document.querySelector("#totalKm").value),
         toNumber(document.querySelector("#custoManut").value),
         toNumber(document.querySelector("#mediaCons").value),
         toNumber(document.querySelector("#custoCombus").value),
     ]
-}
+};
 
-document.querySelector("#fatur").addEventListener("focus", function () {
-    this.select();
-});
+function calc() {
+    const [ganhos, totalKm, custoManut, mediaCons, custoCombus] = getValues();
 
-document.querySelector("#fatur").addEventListener("blur", function () {
-    const string = this.value.replaceAll(",", ".");
+    const string = ganhos.replaceAll(",", ".");
     const values = string.split(" ");
-    const total = values.reduce((soma, valor) =>
+    const totalBruto = values.reduce((soma, valor) =>
         soma += isNaN(parseFloat(valor)) ? 0 : parseFloat(valor), 0)
         .toLocaleString("pt-BR", {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2
         });
-    this.value = total;
-});
 
-function calc() {
-    const [fatur, totalKm, custoManut, mediaCons, custoCombus] = getValues();
-
-    const custoTotalManut =
-        totalKm * custoManut;
-
-    const custoTotalCombus =
-        totalKm / mediaCons * custoCombus;
-
-    fatur - custoTotalManut - custoTotalCombus
-        ? document.querySelector("#result").value =
-        (fatur - custoTotalManut - custoTotalCombus)
-            .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
-        : alert("Impossível Calcular!");
+    document.querySelector("#totalBruto").innerText = "R$ " + totalBruto;
 };
+
 
 document.querySelector("#calcButton")
     .addEventListener("click", calc);
